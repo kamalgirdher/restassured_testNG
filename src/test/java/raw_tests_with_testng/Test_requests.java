27f0aa434df1a78b1b32a1d77f630054eac90ee8
GIRDHER Kamal TGI/OLS <kamal.girdher@orange.com>
2019-05-31 12:33:35 +0530

    added source code

---
+++ src/test/java/raw_tests_with_testng/Test_requests.java
@@ -0,0 +1,12 @@
+package raw_tests_with_testng;
+
+import static io.restassured.RestAssured.given;
+
+import org.testng.annotations.Test;
+
+public class Test_requests {
+	@Test
+	public void test_getRequest() {
+		given().get("http://localhost:3000/answer").then().statusCode(200);
+	}
+}
